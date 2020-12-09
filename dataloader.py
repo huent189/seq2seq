a34cf1c6b4a862ca5c05c566459be3b687139231
huent189 <hueahgase189@gmail.com>
2020-12-09 14:18:44 +0700

    update

--- dataloader.py
+++ dataloader.py
@@ -3,58 +3,70 @@ from pyvi import ViTokenizer
 import os
 import dill
 import torch
+
+
 def tokenize(text):
     text = ViTokenizer.tokenize(text)
     return text.split()
+
+
 def get_dataloader(root_path, split=False, batch_size=8, device='cuda', save_path=None, reload=None):
-    VI = torchtext.data.Field(tokenize=tokenize, 
-            init_token='<sos>', 
-            eos_token='<eos>', 
-            lower=True)
-    EN = torchtext.data.Field(tokenize=tokenize, 
-            init_token='<sos>', 
-            eos_token='<eos>', 
-            lower=True)
-    data_fields= [('en', EN), ('vi', VI)]
+    VI = torchtext.data.Field(tokenize=tokenize,
+                              init_token='<sos>',
+                              eos_token='<eos>',
+                              lower=True)
+    EN = torchtext.data.Field(tokenize=tokenize,
+                              init_token='<sos>',
+                              eos_token='<eos>',
+                              lower=True)
+    data_fields = [('en', EN), ('vi', VI)]
     if split:
-        train_data,val_data = torchtext.data.TabularDataset.splits(path=root_path, train='train.csv', 
-                        validation='val.csv', format='csv', fields=data_fields, skip_header=False)
+        train_data, val_data = torchtext.data.TabularDataset.splits(path=root_path, train='train.csv',
+                                                                    validation='val.csv', format='csv', fields=data_fields, skip_header=False)
         train_iter, val_iter = torchtext.data.BucketIterator.splits(
-                            (train_data, val_data), # we pass in the datasets we want the iterator to draw data from
-                            batch_sizes=(batch_size, batch_size),
-                            device=device, # if you want to use the GPU, specify the GPU number here
-                            sort_key=lambda x: len(x.en), # the BucketIterator needs to be told what function it should use to group the data.
-                            sort_within_batch=False,
-                            shuffle=True # we pass repeat=False because we want to wrap this Iterator layer.
-                            )
+            # we pass in the datasets we want the iterator to draw data from
+            (train_data, val_data),
+            batch_sizes=(batch_size, batch_size),
+            device=device,  # if you want to use the GPU, specify the GPU number here
+            # the BucketIterator needs to be told what function it should use to group the data.
+            sort_key=lambda x: len(x.en),
+            sort_within_batch=False,
+            # we pass repeat=False because we want to wrap this Iterator layer.
+            shuffle=True
+        )
     else:
-        train_data = torchtext.data.TabularDataset(path=root_path, format='csv', fields=data_fields, skip_header=True)
-        train_iter= torchtext.data.BucketIterator(
-                            (train_data, val_data), # we pass in the datasets we want the iterator to draw data from
-                            batch_size=batch_size,
-                            device=device, # if you want to use the GPU, specify the GPU number here
-                            sort_key=lambda x: len(x.en), # the BucketIterator needs to be told what function it should use to group the data.
-                            sort_within_batch=False,
-                            shuffle=True # we pass repeat=False because we want to wrap this Iterator layer.
-                            )
+        train_data = torchtext.data.TabularDataset(
+            path=root_path, format='csv', fields=data_fields, skip_header=True)
+        train_iter = torchtext.data.BucketIterator(
+            # we pass in the datasets we want the iterator to draw data from
+            (train_data, val_data),
+            batch_size=batch_size,
+            device=device,  # if you want to use the GPU, specify the GPU number here
+            # the BucketIterator needs to be told what function it should use to group the data.
+            sort_key=lambda x: len(x.en),
+            sort_within_batch=False,
+            # we pass repeat=False because we want to wrap this Iterator layer.
+            shuffle=True
+        )
     if reload is not None:
-        VI = torch.load(os.path.join(reload,"VI.Field"))
-        EN = torch.load(os.path.join(reload,"EN.Field"))
+        VI = torch.load(os.path.join(reload, "VI.Field"))
+        EN = torch.load(os.path.join(reload, "EN.Field"))
     else:
         print('build vocab')
         VI.build_vocab(train_data)
         EN.build_vocab(train_data)
     if save_path:
-        torch.save(VI, os.path.join(save_path,"VI.Field"))
-        torch.save(EN, os.path.join(save_path,"EN.Field"))
+        torch.save(VI, os.path.join(save_path, "VI.Field"))
+        torch.save(EN, os.path.join(save_path, "EN.Field"))
     if split:
-        return train_iter, val_iter, EN, VI 
+        return train_iter, val_iter, EN, VI
     else:
-        return train_iter, EN, VI  
+        return train_iter, EN, VI
+
+
 if __name__ == "__main__":
-    train, val, en, vi = get_dataloader('/home/dell/Documents/thesis/transformer/data', split=True, device=-1)
+    train, val, en, vi = get_dataloader(
+        '/home/dell/Documents/thesis/transformer/data', split=True, device=-1)
     for i, batch in enumerate(train):
         print(batch)
         break
-
-        
--- train.py
+++ train.py
@@ -2,15 +2,69 @@ import torch
 import argparse
 import os
 from model.transformers import Transformer
+import tqdm
+from dataloader import get_dataloader
+from torch.utils.tensorboard import SummaryWriter
+
+def initialize_weights(m):
+    if hasattr(m, 'weight') and m.weight.dim() > 1:
+        torch.nn.init.xavier_uniform_(m.weight.data)
+
+
+def train_one_epoch(model, iterator, optimizer, criterion, clip, device):
+    model.train()
+    epoch_loss = 0
+    for i, batch in enumerate(tqdm(iterator)):
+        src = batch.en
+        trg = batch.vi
+        trg_input = trg[:, :-1]
+        src = src.to(device)
+        trg = trg.to(device)
+        optimizer.zero_grad()
+        output = model(src, trg_input)
+
+        output = output[1:].view(-1, output.shape[-1])
+        trg = trg[1:].view(-1)
+        loss = criterion(output, trg)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        optimizer.step()
+        epoch_loss += loss.item()
+    return epoch_loss / len(iterator)
+
+
+def evaluate(model, iterator, criterion):
+    model.eval()
+    epoch_loss = 0
+    with torch.no_grad():
+        for i, batch in enumerate(tqdm(iterator)):
+            src = batch.en
+            trg = batch.vi
+            output = model(src, trg)  # turn off teacher forcing
+            output = output[1:].view(-1, output.shape[-1])
+            trg = trg[1:].view(-1)
+            loss = criterion(output, trg)
+            epoch_loss += loss.item()
+    return epoch_loss / len(iterator)
+
+
 def train(config):
-    vc = 200 # todo:
-    os.environ['CUDA_VISIBLE_DEVICES']=config.gpu_id
-    model = Transformer(vc)
+    os.environ['CUDA_VISIBLE_DEVICES'] = config.gpu_id
+    writer = SummaryWriter(log_dir=config.log_dir)
+    train, val, en, vi = get_dataloader(config.data_dir, split=True, batch_sizea=config.batch_size, device=config.gpu_id)
+    pad_idx = vi.vocab.stoi[vi.pad_token]
+    model = Transformer(len(en))
     if config.gpu_id != -1:
         model = model.cuda()
+    model.apply(initialize_weights)
     if config.pretrain_dir != "":
         model.load_state_dict(torch.load(config.pretrain_dir))
-    optimizer = torch.optim.Adam(model.parameters(),)
+    critetion = torch.nn.CrossEntropyLoss(ignore_index=pad_idx)
+    # todo warm up cool down lr
+    optimizer = torch.optim.Adam(model.parameters(), betas=[
+                                 0.9, 0.98], lr=config.lr)
+
+    # write log
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default="data")
@@ -25,7 +79,6 @@ if __name__ == "__main__":
     parser.add_argument('--display_iter', type=int, default=10)
     parser.add_argument('--snapshot_iter', type=int, default=10)
     parser.add_argument('--snapshots_folder', type=str, default="snapshot")
-    parser.add_argument('--pretrain_dir', type=str, default= "")
+    parser.add_argument('--pretrain_dir', type=str, default="")
     parser.add_argument('--gpu_id', type=str, default='0')
     config = parser.parse_args()
-
