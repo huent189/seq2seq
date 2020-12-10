4f237ad3ef8846691ce620692e9efbda6c4026d3
huent189 <hueahgase189@gmail.com>
2020-12-11 03:30:12 +0700

    update

--- dataloader.py
+++ dataloader.py
@@ -1,20 +1,24 @@
 import torchtext
 from pyvi import ViTokenizer
 import os
-# import dill
+import dill
 import torch
 from torchtext.data.utils import get_tokenizer
 import html
 def vi_tokenize(text):
     text = ViTokenizer.tokenize(text)
-    return text.split()
+    text = text.split()
+    # print(text)
+    return text
 
 def en_tokenize(text):
     # text = html.unescape(text)
     # return get_tokenizer('spacy')(text)
-    return text.split()
+    text = text.split()
+    # print(text)
+    return text
 
-def get_dataloader(root_path, split=False, batch_size=8, device='cuda', save_path=None, reload=None):
+def get_dataloader(root_path, train_file="train.csv", test_file="test.csv", batch_size=8, device='cuda', save_path=None, reload=None):
     VI = torchtext.data.Field(tokenize=vi_tokenize,
                               init_token='<sos>',
                               eos_token='<eos>',
@@ -28,48 +32,29 @@ def get_dataloader(root_path, split=False, batch_size=8, device='cuda', save_pat
                               fix_length=200,
                               batch_first=True)
     data_fields = [('en', EN), ('vi', VI)]
-    if split:
-        train_data, val_data = torchtext.data.TabularDataset.splits(path=root_path, train='train.csv',
-                                                                    validation='val.csv', format='csv', fields=data_fields, skip_header=True)
-        train_iter, val_iter = torchtext.data.BucketIterator.splits(
-            # we pass in the datasets we want the iterator to draw data from
-            (train_data, val_data),
-            batch_sizes=(batch_size, batch_size),
-            device=device,  # if you want to use the GPU, specify the GPU number here
-            # the BucketIterator needs to be told what function it should use to group the data.
-            sort_key=lambda x: len(x.en),
-            sort_within_batch=False,
-            # we pass repeat=False because we want to wrap this Iterator layer.
-            shuffle=True
-        )
-    else:
-        train_data = torchtext.data.TabularDataset(
-            path=root_path, format='csv', fields=data_fields, skip_header=True)
-        train_iter = torchtext.data.BucketIterator(
-            # we pass in the datasets we want the iterator to draw data from
-            (train_data, val_data),
-            batch_size=batch_size,
-            device=device,  # if you want to use the GPU, specify the GPU number here
-            # the BucketIterator needs to be told what function it should use to group the data.
-            sort_key=lambda x: len(x.en),
-            sort_within_batch=False,
-            # we pass repeat=False because we want to wrap this Iterator layer.
-            shuffle=True
-        )
+    train_data, val_data = torchtext.data.TabularDataset.splits(path=root_path, train=train_file, validation=test_file, format='csv', fields=data_fields, skip_header=True)
+    train_iter, val_iter = torchtext.data.BucketIterator.splits(
+        # we pass in the datasets we want the iterator to draw data from
+        (train_data, val_data),
+        batch_sizes=(batch_size, batch_size),
+        device=device,  # if you want to use the GPU, specify the GPU number here
+        # the BucketIterator needs to be told what function it should use to group the data.
+        sort_key=lambda x: len(x.en),
+        sort_within_batch=False,
+        # we pass repeat=False because we want to wrap this Iterator layer.
+        shuffle=True
+    )
     if reload is not None:
-        VI = torch.load(os.path.join(reload, "VI.Field"))
-        EN = torch.load(os.path.join(reload, "EN.Field"))
+        VI = torch.load(os.path.join(reload, "VI.Field"), pickle_module=dill)
+        EN = torch.load(os.path.join(reload, "EN.Field"), pickle_module=dill)
     else:
         print('build vocab')
         VI.build_vocab(train_data)
         EN.build_vocab(train_data)
     if save_path:
-        torch.save(VI, os.path.join(save_path, "VI.Field"))
-        torch.save(EN, os.path.join(save_path, "EN.Field"))
-    if split:
-        return train_iter, val_iter, EN, VI
-    else:
-        return train_iter, EN, VI
+        torch.save(VI, os.path.join(save_path, "VI.Field"), pickle_module=dill)
+        torch.save(EN, os.path.join(save_path, "EN.Field"), pickle_module=dill)
+    return train_iter, val_iter, EN, VI
 
 
 if __name__ == "__main__":
