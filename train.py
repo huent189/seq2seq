be4abcf6ffa57e07627939dc1f65ba9194859b7e
huent189 <hueahgase189@gmail.com>
2020-12-10 11:02:53 +0700

    rename arg

--- train.py
+++ train.py
@@ -73,8 +73,8 @@ def train(config):
     model = Transformer(max(len(en.vocab.stoi), len(vi.vocab.stoi)))
     model = model.to(device)
     model.apply(initialize_weights)
-    if config.pretrain_dir != "":
-        model.load_state_dict(torch.load(config.pretrain_dir))
+    if config.pretrain_model != "":
+        model.load_state_dict(torch.load(config.pretrain_model))
     criterion = torch.nn.CrossEntropyLoss(ignore_index=pad_idx)
     # todo warm up cool down lr
     optimizer = torch.optim.Adam(model.parameters(), betas=[
@@ -105,10 +105,10 @@ if __name__ == "__main__":
     parser.add_argument('--num_epochs', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--num_workers', type=int, default=4)
-    parser.add_argument('--display_iter', type=int, default=10)
+    parser.add_argument('--display_iter', type=int, default=40)
     parser.add_argument('--snapshot_iter', type=int, default=10)
     parser.add_argument('--snapshots_folder', type=str, default="snapshot")
-    parser.add_argument('--pretrain_dir', type=str, default="")
+    parser.add_argument('--pretrain_model', type=str, default="")
     parser.add_argument('--gpu_id', type=str, default='0')
     config = parser.parse_args()
     train(config)
